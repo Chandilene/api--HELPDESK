015c25aed46ce91b2e00b97a85d87daa9195bb49
Chandilene <chandileneborges@gmail.com>
2026-05-25 16:27:31 -0300

    feat: .

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -2,7 +2,6 @@ import multer from "multer";
 import path from "node:path";
 import crypto from "node:crypto";
 import fs from "fs";
-// import { fileURLToPath } from "url";
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);

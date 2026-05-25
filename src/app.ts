1e2bd74b7c9cdd5f7f8391a55525312bec1dd0bb
Chandilene <chandileneborges@gmail.com>
2026-05-25 16:55:20 -0300

    feat: removendo arquivo upload  não utilizado

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
-import { UPLOADS_FOLDER } from "./configs/upload";
+
 import { errorHandling } from "@/middlewares/error-handling";
 import { routes } from "@/routes";
 
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use("/files", express.static(UPLOADS_FOLDER));
+
 app.use(routes);
 
 app.use(errorHandling);
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -3,12 +3,9 @@ import { UsersController } from "@/controllers/UsersController";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
-import multer from "multer";
-import { MULTER } from "@/configs/upload";
 import { AvatarUserController } from "@/controllers/AvatarUserController";
 
 const avatarUserController = new AvatarUserController();
-const upload = multer(MULTER);
 
 const usersRoutes = Router();
 const usersController = new UsersController();
